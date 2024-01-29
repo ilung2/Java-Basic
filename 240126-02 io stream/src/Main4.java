c736b1c7ed933c8af75ae77ba673e4cf731a6020
ilung2 <doqjjong4@naver.com>
2024-01-29 18:04:32 +0900

    240126-02 io stream

---
+++ 240126-02 io stream/src/Main4.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main4 {
+	public static void main(String[] args) {
+		String line = "Text Line";
+		System.out.println(line);
+		
+		byte[] arr = line.getBytes();
+		System.out.println(Arrays.toString(arr));
+		
+		String kor = "한글 정보";
+		byte[] arr2 = kor.getBytes();
+		System.out.println(Arrays.toString(arr2));
+		 
+		System.out.println(new String(arr)); // 생성자에다가 byte배열
+		System.out.println(new String(arr2));
+	}
+}
---
+++ 240126-02 io stream/src/Main8.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main8 {
+	public static void main(String[] args) {
+		File origin = new File("data.txt");
+		File copy = new File("copy.txt");
+		
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		try {
+			fis = new FileInputStream(origin);
+			fos = new FileOutputStream(copy);
+			
+			int length = (int) origin.length();
+			byte[] bytes = new byte[length];
+			
+			fis.read(bytes);
+			fos.write(bytes);
+			 
+			System.out.println("복사가 완료되었습니다.");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
